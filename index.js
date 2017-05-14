7043a4f0e6442aaf7651424637eacfeb9bbe8b57
ColinYergin <yergicol@umich.edu>
2017-05-14 16:30:01 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,132 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var server = express();
+var port = 8080;
+
+var activeGames = {};
+var validColors = {
+	white:  ()=>"white",
+	black:  ()=>"black",
+	random: ()=>Math.random()>.5?"white":"black",
+};
+function makeKeyPair() {
+	var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	var s1 = "a";
+	var s2 = "b";
+	for(var i = 0; i < 16; ++i) {
+		s1 = s1 + alphabet.charAt(Math.floor(Math.random()*alphabet.length));
+		s2 = s2 + alphabet.charAt(Math.floor(Math.random()*alphabet.length));
+	}
+	return [s1, s2];
+}
+
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+server.use(express.static(path.join(__dirname, '/static')))
+
+server.get('/', function(req, res) {
+	console.log(req.url);
+	res.sendFile('index.html', {root: __dirname});
+});
+
+server.put('/NewGame', function(req, res) {
+	console.log(req.body);
+	if(activeGames[req.name]) {
+		res.json({error:"Please choose a different game name"});
+	} else if(validColors[req.body.color] === undefined) {
+		res.json({error:"Invalid color"});
+	} else {
+		var color = validColors[req.body.color]();
+		var keys = makeKeyPair()
+		var gameobj = {
+			moves:[],
+			wkey: keys[0],
+			bkey: keys[1],
+			joined: false,
+			creator: color,
+			listeners: [],
+		};
+		activeGames[req.body.name] = gameobj;
+		res.json({
+			setup: "new",
+			player:color,
+			key:   color==="white"?gameobj.wkey:gameobj.bkey,
+			moves: [],
+			name: req.body.name,
+		});
+	}
+});
+
+server.put('/JoinGame', function(req, res) {
+	console.log(req.body);
+	var game = activeGames[req.body.name];
+	if(game && !game.joined) {
+		game.joined = true;
+		res.json({
+			setup: "new",
+			player:game.creator==="white"?"black":"white",
+			key:   game.creator==="white"?game.bkey:game.wkey,
+			moves: game.moves,
+			name: req.body.name,
+		});
+	} else if(game) {
+		res.json({error:"This game has already been joined"});
+	} else {
+		res.json({error:"No game with that name exists"});
+	}
+});
+
+function validateGameReq(req, res) {
+	var game = activeGames[req.body.name];
+	if(!game) {
+		res.json({error:"No game with that name exists"});
+	} else if((req.body.color==="white"?game.wkey:game.bkey) !== req.body.key) {
+		res.json({error:"Incorrect key"});
+	}
+	return game;
+}
+
+function isValidMove(game, move) {
+	return true;
+}
+
+function getTurn(game) {
+	var order = ["w", "b", "b", "w"];
+	return order[game.moves.length % order.length]
+}
+
+function alertListeners(game) {
+	game.listeners.forEach(l => {
+		l.json({moves:game.moves});
+	});
+	game.listeners = [];
+}
+
+server.put('/Listen', function(req, res) {
+	console.log(req.body);
+	var game = validateGameReq(req, res);
+	if(req.body.movenum < game.moves.length) {
+		res.json({moves:game.moves});
+	} else {
+		game.listeners.push(res);
+		console.log("added listener");
+	}
+});
+
+server.put('/Move', function(req, res) {
+	console.log(req.body);
+	var game = validateGameReq(req, res);
+	if(isValidMove(game, req.body.move)) {
+		game.moves.push(req.body.move);
+		alertListeners(game);
+		res.json({success: true});
+	} else {
+		res.json({error:"Invalid move"});
+	}
+});
+
+server.listen(port, function() {
+	console.log('server listening on port ' + port);
+});
---
+++ static/scripts.js
@@ -0,0 +1,333 @@
+var players = {
+	b: "Random",
+	w: "Human"
+};
+var Agents = {
+	Human(c) {UIoptions = enumMoves(c);drawUIOptions();},
+	Random(c) {setTimeout(()=>RandomAgent(c),100);},
+	Network(c) {setTimeout(()=>StartListening(),0);}
+};
+var networkInfo = undefined;
+var UIoptions = [];
+var board = {
+	height: 5,
+	width: 5,
+	turns: ["w", "b", "b", "w"],
+	get turn() {return this.turns[this.turnind % this.turns.length];},
+	get nextturn() {return this.turns[(this.turnind+1)%this.turns.length];},
+	get prevturn() {return this.turns[(this.turnind+this.turns.length-1)%this.turns.length];},
+	turnind: 0,
+	inRange(x, y) {return x >= 0 && x < this.width && y >= 0 && y < this.height;},
+	get: function(x, y) {return this[x+","+y];},
+	set: function(x, y, c) {
+		this[x+","+y] = c;
+		var square = document.getElementById("s"+x+","+y);
+		if(square.querySelector(".circle")) {
+			square.removeChild(square.querySelector(".circle"));
+		}
+		if(c) {
+			var piece = document.createElement("DIV");
+			piece.className = c + " circle";
+			square.appendChild(piece);
+		}
+		this.recordScore();
+	},
+	triggerNextTurn: function() {
+		Agents[players[this.turn]](this.turn);
+	},
+	doturn: function(action, trigger) {
+		if(players[this.turn === "b"?"w":"b"] === "Network" && players[this.turn] === "Human") {
+			SendMove(action);
+		}
+		if(action.type === "move") {
+			this.set(...action.from, undefined);
+			this.set(...action.to, this.turn);
+		} else if(action.type === "make") {
+			this.set(...action.loc, this.turn);
+		}
+		this.turnind = this.turnind + 1;
+		this.recordScore();
+		if(trigger || trigger === undefined) this.triggerNextTurn();
+	},
+	get_state: function() {
+		kv = [];
+		this.each((e, x, y)=>kv.push([x+","+y, e]));
+		kv.push(["turn", this.turn])
+		return kv;
+	},
+	set_state: function(kv) {
+		var x = this;
+		kv.forEach(a=>x[a[0]]=a[1]);
+		return this;
+	},
+	setup: function() {
+		var mid = (this.width-1)/2;
+		this.each((c,x,y)=> this.set(x,y,undefined));
+		this.set(Math.round(mid-1), 0, "w");
+		this.set(Math.round(mid+1), 0, "w");
+		this.set(Math.round(mid-1), this.height-1, "b");
+		this.set(Math.round(mid+1), this.height-1, "b");
+		UIoptions = [];
+		drawUIOptions();
+		this.recordScore();
+	},
+	recordScore: function() {
+		document.getElementById("wscore").innerHTML = this.score("w");
+		document.getElementById("bscore").innerHTML = this.score("b");
+	},
+	score: function(c) {
+		count = 0;
+		this.each(x=>count += x===c?1:0);
+		return count;
+	},
+	each: function(cb) {
+		for(var y = 0; y < board.height; ++y) {
+			for(var x = 0; x < board.width; ++x) {
+				cb(this.get(x, y), x, y);
+			}
+		}
+	},
+	get str() {
+		var str = this.turn + this.nextturn;
+		for (var y = board.height - 1; y >= 0; y--) {
+			for (var x = board.width - 1; x >= 0; x--) {
+				str += this.get(x, y);
+			}
+		}
+		return str;
+	},
+};
+
+window.onload = () => {
+	var boarddiv = document.getElementById("board");
+	var tbl = document.createElement("TABLE");
+	tbl.id = "boardTable";
+	tbl.className = "eventbl";
+	boarddiv.appendChild(tbl);
+	for(var y = 0; y < board.height * 2; ++y) {
+		var row = document.createElement("TR");
+		row.id = "br"+y;
+		row.className = "boardRow";
+		tbl.appendChild(row);
+		for(var x = 0; x < board.width * 2; ++x) {
+			var square = document.createElement("TD");
+			square.className = "bcell"+(x%2?" lbord ":"")+(y%2?" ubord":"");
+			square.id = "bs"+x+","+y;
+			row.appendChild(square);
+		}
+	}
+	var tbl2 = document.createElement("TABLE");
+	tbl2.id = "piecesTable";
+	tbl2.className = "eventbl";
+	boarddiv.appendChild(tbl2);
+	for(var y = 0; y < board.height; ++y) {
+		var row = document.createElement("TR");
+		row.id = "r"+y;
+		row.className = "pieceRow";
+		tbl2.appendChild(row);
+		for(var x = 0; x < board.width; ++x) {
+			var square = document.createElement("TD");
+			square.className = "pieceCell";
+			square.id = "s"+x+","+y;
+			row.appendChild(square);
+			square.addEventListener("click", clickHandler(x,y));
+		}
+	}
+	board.setup();
+	Agents[players[board.turn]](board.turn);
+};
+
+function drawUIOptions() {
+	document.querySelectorAll(".optind").forEach(o=>o.parentNode.removeChild(o));
+	UIoptions.forEach(m => {
+		var square = document.getElementById("s"+m.ui[0][0]+","+m.ui[0][1]);
+		if(!square.querySelector(".optind")) {
+			var optind = document.createElement("DIV");
+			optind.className = "optind";
+			square.appendChild(optind);
+		}
+	})
+}
+
+function recordError(e) {
+	document.getElementById("lasterror").innerHTML = e;
+}
+
+function clickHandler(x, y) {
+	return e => {
+		if(players[board.turn] === "Human") {
+			var newopts = UIoptions.filter(m => m.ui[0].join() === [x,y].join());
+			if(newopts.length > 0) {
+				newopts.forEach(m => m.ui.shift());
+				UIoptions = newopts;
+				if(UIoptions.length === 1 && UIoptions[0].ui.length === 0) {
+					UIoptions = [];
+					board.doturn(newopts[0]);
+				}
+			} else if(newopts.length === 0) {
+				UIoptions = enumMoves(board.turn);
+			}
+			drawUIOptions();
+		}
+	}
+}
+
+function RandomAgent(c) {
+	var moves = enumMoves(c);
+	if(moves.length === 0) {
+		console.log("No moves");
+	} else {
+		board.doturn(moves[Math.floor(Math.random()*moves.length)]);
+	}
+}
+
+function enumMoves(c) {
+	var moves = [];
+	for(var x = 0; x < board.width; ++x) {
+		for(var y = 0; y < board.height; ++y) {
+			if(board.inRange(x, y) && !board.get(x, y)) {
+				adj = [[-1,0],[1,0],[0,-1],[0,1]].map(c=>[c[0]+x,c[1]+y]);
+				adj.forEach(coords => {
+					if(board.get(...coords) === c) {
+						moves.push({
+							type:"move",
+							from: coords,
+							to: [x, y],
+							ui: [coords, [x, y]],
+						});
+					}
+				});
+			}
+			if((board.get(x+1,y) === c && board.get(x-1,y) === c && board.get(x,y) !== c) || 
+			   (board.get(x,y+1) === c && board.get(x,y-1) === c && board.get(x,y) !== c)) {
+				moves.push({
+					type:"make",
+					loc:[x,y],
+					ui:[[x,y]],
+					takes: board.get(x,y),
+				});
+			}
+		}
+	}
+	return moves;
+}
+
+function SetupObject(res) {
+	if(res.setup === "new") {
+		var colors = {white:"w", black:"b"};
+		var othercolor = {white:"b", black:"w"};
+		players[colors[res.player]] = "Human";
+		players[othercolor[res.player]] = "Network";
+		board.setup();
+		networkInfo = {
+			key: res.key,
+			mycolor: colors[res.player],
+			color: res.player,
+			name: res.name
+		}
+		res.moves.forEach(m => board.doturn(m));
+		board.turnind = 0;
+		board.triggerNextTurn();
+		document.getElementById("NewGame").hidden = true;
+		document.getElementById("GameInfo").hidden = false;
+		document.getElementById("lasterror").innerHTML = "";
+		document.getElementById("game_info_name").innerHTML = res.name;
+		document.getElementById("game_info_color").innerHTML = networkInfo.color;
+	}
+}
+
+function StartListening() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', 'Listen');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState === XMLHttpRequest.DONE) {
+			if(xhr.status === 200) {
+				var res = JSON.parse(xhr.responseText);
+				if(res.error) {
+					recordError(res.error);
+				} else {
+					for(var i = board.turnind; i < res.moves.length; ++i) {
+						board.doturn(res.moves[i], i+1===res.moves.length);
+					}
+				}
+			} else {
+				console.log("failed");
+				setTimeout(StartListening, 0);
+			}
+		}
+	};
+	xhr.send(JSON.stringify({
+		color: networkInfo.color,
+		name: networkInfo.name,
+		key: networkInfo.key,
+		movenum: board.turnind,
+	}));
+}
+
+function SendMove(move) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', 'Move');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState === XMLHttpRequest.DONE) {
+			if(xhr.status === 200) {
+				var res = JSON.parse(xhr.responseText);
+				if(res.error) {
+					recordError(res.error);
+				} else {
+					console.log("sent move");
+				}
+			} else {
+				recordError("Couldn't send move, retrying");
+				setTimeout(() => SendMove(move), 0);
+			}
+		}
+	};
+	xhr.send(JSON.stringify({
+		color: networkInfo.color,
+		name: networkInfo.name,
+		key: networkInfo.key,
+		move: move,
+	}));
+}
+
+function NewGameFormSubmit() {
+	var color = document.querySelector('input[name=color]:checked').value;
+	var name = document.getElementById("GameName").value;
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', 'NewGame');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState === XMLHttpRequest.DONE) {
+			if(xhr.status === 200) {
+				var res = JSON.parse(xhr.responseText);
+				if(res.error) recordError(res.error);
+				else SetupObject(res);
+			} else {
+				recordError("Couldn't start game");
+			}
+		}
+	};
+	xhr.send(JSON.stringify({color:color, name:name}));
+}
+
+function JoinGameFormSubmit() {
+	var color = document.querySelector('input[name=color]:checked').value;
+	var name = document.getElementById("GameName").value;
+	var xhr = new XMLHttpRequest();
+	xhr.open('PUT', 'JoinGame');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.onreadystatechange = function() {
+		if(xhr.readyState === XMLHttpRequest.DONE) {
+			if(xhr.status === 200) {
+				var res = JSON.parse(xhr.responseText);
+				if(res.error) recordError(res.error);
+				else SetupObject(res);
+			} else {
+				recordError("Couldn't join game");
+			}
+		}
+	};
+	xhr.send(JSON.stringify({color:color, name:name}));
+}
